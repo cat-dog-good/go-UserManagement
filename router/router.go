897e649b2febc0038013239ac5b0145d5e100efd
qll-NO.1 <zyj647@126.com>
2026-05-29 16:33:13 +0800

    feat: 跑通ping

---
+++ router/router.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	return r
+}
